ea0717db476622bc4bc52f1fe5ab7f9ce818aaf1
zhangbo <zhangbo@hzzl.net>
2022-06-21 17:19:37 +0800

    实现byName和byType

--- src/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-import cn.self.zhangbo.service.IndexService;
-import cn.self.zhangbo.service.impl.IndexServiceImpl;
-
-public class MainTest {
-
-    public static void main(String[] args) {
-        System.out.println(IndexService.class.isInstance(new IndexServiceImpl()));
-    }
-}
